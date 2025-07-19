34f57340c11990736e137b3fda59af151bb4a952
achdp07 <layefils26@gmail.com>
2025-07-19 23:56:14 +0200

    Learning reactjs

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,21 @@
+function USerProfile(UserContext) {
+    return (
+        UserContext.name && UserContext.age && UserContext.bio ? (
+            <UserProfile name={UserContext.name} age={UserContext.age} bio={UserContext.bio} />
+        ) : (
+            <p>Please provide valid user information.</p>
+        )
+    )
+}
+
+ const UserProfile = (UserContext) => {
+   return (
+     <div>
+       <h2>{UserContext.name}</h2>
+       <p>Age: {UserContext.age}</p>
+       <p>Bio: {UserContext.bio}</p>
+     </div>
+   );
+ };
+
+export default USerProfile;
