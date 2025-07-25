6be4225eb72f38dd53243221ce13cec9f22428ec
achdp07 <layefils26@gmail.com>
2025-07-25 22:44:55 +0200

    adding new components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import { useRecipeStore } from './components/recipeStore';    
+import { useRecipeStore } from './components/recipeStore';
 
 function App() {
   
@@ -13,6 +13,16 @@ function App() {
      <div>
       <RecipeList />
       <AddRecipeForm  />
+      <EditRecipeForm recipeId={1} />
+      {/* Assuming EditRecipeForm is defined and imported */}
+      <RecipeDetails recipeId={1} />
+      {/* Assuming RecipeDetails is defined and imported */}
+      <h1>Recipe Sharing App</h1>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
      </div>
     </>
   )
