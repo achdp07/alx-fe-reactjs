0a487d15c2ec6a81cb2426b9d814cb9f677eaddd
achdp07 <layefils26@gmail.com>
2025-07-25 22:56:55 +0200

    Updated the zustand Store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,8 +2,24 @@ import creaate from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
+
+  // Add a new recipe
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+
+  // Edit an existing recipe
+  SetRecipe: (recipes) => set({ recipes }),
+
+  // Delete a recipe
+  deleteRecipe: (Id) => set(state => ({ 
+    recipes: state.recipes.filter(recipe => recipe.id !== Id),
+  }),
+
+  // Update an existing recipe by ID
+  updateRecipe: (updatedRecipe) => set(state => ({
+  recipes: state.recipes.map(recipe =>
+    recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+  ),
+  }));
 }));
 
 export default useRecipeStore;
