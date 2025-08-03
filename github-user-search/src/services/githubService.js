dfd3fdf10f0d017cf18992118731b896c2074409
achdp07 <layefils26@gmail.com>
2025-08-03 23:47:35 +0200

    Updated Search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export const fetchUser = async (username) => {
+export const fetchUserData = async (username) => {
   const res = await fetch(`https://api.github.com/users/${username}`, {
     headers: {
       Authorization: `token ${apiKey}`
