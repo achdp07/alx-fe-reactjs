36382d4e2c2b7138f6a35681119e423b48b32657
achdp07 <layefils26@gmail.com>
2025-08-03 23:34:07 +0200

    Updated search file

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css'
-import Search from './components/search'
+import Search from './components/Search'
 
 
 function App() {
