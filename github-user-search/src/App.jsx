eb929b60873ce16234738383daf327b04b7b92f0
achdp07 <layefils26@gmail.com>
2025-08-03 23:44:00 +0200

    Search updated

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import Navbar from './components/Navbar' 
 import Search from './components/Search'
 
 
