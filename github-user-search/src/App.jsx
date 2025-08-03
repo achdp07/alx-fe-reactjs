befc073fd1dc61dc7ad08c9e7b5df544d359713f
achdp07 <layefils26@gmail.com>
2025-08-03 23:29:58 +0200

    Added Search Components

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import Search from './components/search'
 
 
 function App() {
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 export const fetchUser = async (username) => {
@@ -8,3 +9,5 @@ export const fetchUser = async (username) => {
   });
   return res.json();
 };
+
+
