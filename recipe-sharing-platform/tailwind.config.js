2e25314f039883991abe52eec5c656e566ae15a9
achdp07 <layefils26@gmail.com>
2025-08-15 21:12:36 +0200

    set up Tailwind CSS configuration for a React project    plugins: [],    darkMode: 'class', // Enable dark mode support

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // You can set it to 'media' or 'class' if needed
+  Media: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
