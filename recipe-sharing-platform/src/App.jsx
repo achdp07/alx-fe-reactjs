d5c584c62e76585176ed56c7e974cb941a4d3a44
achdp07 <layefils26@gmail.com>
2025-08-17 23:53:36 +0200

    RecipeDetails comp

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HomePage from './components/HomePage'
+import RecipeDetails from './components/RecipeDetails'
 // "react-router-dom", "BrowserRouter", "Route", "Routes", "RecipeDetail"
 
 const App = () => {
   return (
     <div>
       <HomePage />
+      <RecipeDetails />
     </div>
   )
 }
---
+++ recipe-sharing-platform/src/components/RecipeDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecipeDetails = () => {
+  return (
+    <div className='text-white'>RecipeDetails</div>
+  )
+}
+
+export default RecipeDetails
