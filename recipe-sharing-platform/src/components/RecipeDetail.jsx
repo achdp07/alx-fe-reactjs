7efa092c95b947de0f9a842a53db1421a9fa74b5
achdp07 <layefils26@gmail.com>
2025-08-17 23:58:50 +0200

    Update

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -5,6 +5,7 @@ const RecipeDetails = () => {
     <div className='text-white'>RecipeDetails
     {/* "useEffect", "useParams", "data.json", "ingredients", "instructions", "image" */}
     {/* "shadow", "font-" */}
+    {/* "Link", "to" */}
     </div>
   )
 }
